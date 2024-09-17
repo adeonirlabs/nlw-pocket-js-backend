6153eeb98ee9b709fb68892900914d529df96c72
Adeonir Kohl <adeonir@gmail.com>
2024-09-17 08:41:23 -0300

    Install and setup fastify cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
+import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
 import {
   type ZodTypeProvider,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { env } from './env'
 import { createGoalRoute } from './routes/create-goal'
 import { getWeekPendingGoalsRoute } from './routes/get-week-pending-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
 import { markGoalCompletedRoute } from './routes/mark-goal-completed'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: env.FRONTEND_URL,
+})
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
